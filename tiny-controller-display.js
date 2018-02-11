51c9219a865d317a68275f1e1985341dc6b935f1
Giancarlo Saraceni <ghsaraceni@yahoo.com>
2018-02-11 10:14:34 -0700

    changed .pngs into .gifs

--- tiny-controller-display.js
+++ tiny-controller-display.js
@@ -2,49 +2,49 @@ var canvas = document.getElementById("controllerCanvas");
 var context = canvas.getContext("2d");
 
 var bottomButton = new Image();
-bottomButton.src = "sprites/bottom_button.png";
+bottomButton.src = "sprites/bottom_button.gif";
 
 var dpad = new Image();
-dpad.src = "sprites/dpad.png";
+dpad.src = "sprites/dpad.gif";
 
 var faceplate = new Image();
-faceplate.src = "sprites/faceplate.png";
+faceplate.src = "sprites/faceplate.gif";
 
 var leftBumper = new Image();
-leftBumper.src = "sprites/left_bumper.png";
+leftBumper.src = "sprites/left_bumper.gif";
 
 var leftButton = new Image();
-leftButton.src = "sprites/left_button.png";
+leftButton.src = "sprites/left_button.gif";
 
 var leftSticktop = new Image();
-leftSticktopPressed.src = "sprites/left_sticktop_pressed.png";
+leftSticktopPressed.src = "sprites/left_sticktop_pressed.gif";
 
 var leftStickwellPressed = new Image();
-leftStickwellPressed.src = "sprites/left_stickwell_pressed.png";
+leftStickwellPressed.src = "sprites/left_stickwell_pressed.gif";
 
 var rightTrigger = new Image();
-rightTrigger.src = "sprites/right_trigger.png";
+rightTrigger.src = "sprites/right_trigger.gif";
 
 var rightBumper = new Image();
-rightBumper.src = "sprites/right_bumper.png";
+rightBumper.src = "sprites/right_bumper.gif";
 
 var rightButton = new Image();
-rightButton.src = "sprites/right_button.png";
+rightButton.src = "sprites/right_button.gif";
 
 var rightSticktop = new Image();
-rightSticktopPressed.src = "sprites/right_sticktop_pressed.png";
+rightSticktopPressed.src = "sprites/right_sticktop_pressed.gif";
 
 var rightStickwellPressed = new Image();
-rightStickwellPressed.src = "sprites/right_stickwell_pressed.png";
+rightStickwellPressed.src = "sprites/right_stickwell_pressed.gif";
 
 var rightTrigger = new Image();
-rightTrigger.src = "sprites/right_trigger.png";
+rightTrigger.src = "sprites/right_trigger.gif";
 
 var selectButton = new Image();
-selectButton.src = "sprites/select_button.png";
+selectButton.src = "sprites/select_button.gif";
 
 var startButton = new Image();
-startButton.src = "sprites/start_button.png";
+startButton.src = "sprites/start_button.gif";
 
 var topButton = new Image();
-topButton.src = "sprites/top_button.png";
+topButton.src = "sprites/top_button.gif";
