43ccf7e3c2ae40355634badd7dca74dee294e345
Giancarlo Saraceni <ghsaraceni@yahoo.com>
2018-02-11 10:03:20 -0700

    variable declarations, all sprite images in place

---
+++ tiny-controller-display.js
@@ -0,0 +1,50 @@
+var canvas = document.getElementById("controllerCanvas");
+var context = canvas.getContext("2d");
+
+var bottomButton = new Image();
+bottomButton.src = "sprites/bottom_button.png";
+
+var dpad = new Image();
+dpad.src = "sprites/dpad.png";
+
+var faceplate = new Image();
+faceplate.src = "sprites/faceplate.png";
+
+var leftBumper = new Image();
+leftBumper.src = "sprites/left_bumper.png";
+
+var leftButton = new Image();
+leftButton.src = "sprites/left_button.png";
+
+var leftSticktop = new Image();
+leftSticktopPressed.src = "sprites/left_sticktop_pressed.png";
+
+var leftStickwellPressed = new Image();
+leftStickwellPressed.src = "sprites/left_stickwell_pressed.png";
+
+var rightTrigger = new Image();
+rightTrigger.src = "sprites/right_trigger.png";
+
+var rightBumper = new Image();
+rightBumper.src = "sprites/right_bumper.png";
+
+var rightButton = new Image();
+rightButton.src = "sprites/right_button.png";
+
+var rightSticktop = new Image();
+rightSticktopPressed.src = "sprites/right_sticktop_pressed.png";
+
+var rightStickwellPressed = new Image();
+rightStickwellPressed.src = "sprites/right_stickwell_pressed.png";
+
+var rightTrigger = new Image();
+rightTrigger.src = "sprites/right_trigger.png";
+
+var selectButton = new Image();
+selectButton.src = "sprites/select_button.png";
+
+var startButton = new Image();
+startButton.src = "sprites/start_button.png";
+
+var topButton = new Image();
+topButton.src = "sprites/top_button.png";
